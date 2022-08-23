42208b92d56faeeb4c042b6932afac36546c23e3
takase <takase@jig.jp>
2022-08-23 11:30:45 +0900

    Revert "Merge pull request #3 from jigintern/react"
    This reverts commit 14be22345a3433eff068709547ff7907ef281d30, reversing
    changes made to d3e939c9139100ad208526d5e598bb0e5638ed6a.

--- hoge/unocss.config.ts
+++
@@ -1,7 +0,0 @@
-import presetWind from "@unocss/preset-wind.ts";
-import type { UserConfig } from "@unocss/core.ts";
-
-// @ref https://github.com/unocss/unocss#configurations
-export default <UserConfig> {
-  presets: [presetWind()],
-};
