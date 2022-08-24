6f2fc002a0cf2460a7068838a109616649630acd
kitopito <kitopitowada@gmail.com>
2022-08-24 14:44:52 +0900

    line使えたナリ

--- Map.tsx
+++ Map.tsx
@@ -1,5 +1,6 @@
 import React from "https://cdn.skypack.dev/react@17.0.2?dts";
-import { GoogleMap, LoadScript } from "https://cdn.skypack.dev/@react-google-maps/api?dts";
+import { GoogleMap, LoadScript, Marker, Polyline } from "https://cdn.skypack.dev/@react-google-maps/api?dts";
+import { HomeViewModel } from "./src/view_model/HomeViewModel.ts";
 
 const containerStyle = {
   width: "400px",
@@ -7,11 +8,17 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 35.9522468,
-  lng: 136.1754899,
+  lat: 35.9507182,
+  lng: 136.1825219,
 };
 
+const routePath = [
+  {lat: 35.9522468, lng: 136.1754899},
+  {lat: 35.9507182, lng: 136.1825219}
+]
+
 const Map = () => {
+  const viewModel = new HomeViewModel();
   // このエラーは無視しておｋ
   return (
     <LoadScript googleMapsApiKey="AIzaSyA21hP2aaQA54UNFQFq8he2G0REvzX6Vd4">
@@ -19,7 +26,10 @@ const Map = () => {
         mapContainerStyle={containerStyle}
         center={center}
         zoom={17}
-      ></GoogleMap>
+      >
+        <Marker position={center}></Marker>
+        <Polyline path={routePath}></Polyline>
+      </GoogleMap>
     </LoadScript>
   );
 };
