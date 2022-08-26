c3475e957416282d2dc874c0eb3b2210f3192a41
kitopito <kitopitowada@gmail.com>
2022-08-26 12:13:06 +0900

    stamp rally map

--- src/pages/StampRally.tsx
+++ src/pages/StampRally.tsx
@@ -5,6 +5,32 @@ import { Container, Grid, Header, Label, Button, Image, Dimmer, Loader, Segment,
 import { StampRallyViewModel } from "../view_model/StampRallyViewModel.ts";
 import { HeaderS } from "../component/Header.tsx";
 import { useEffect } from 'https://cdn.skypack.dev/react@17.0.2?dts';
+import { GoogleMap, LoadScript, Polyline, Circle, InfoBox } from "https://cdn.skypack.dev/@react-google-maps/api?dts";
+
+const containerStyle = {
+  width: "100vw",
+  height: "30%",
+};
+
+const center = {
+  lat: 35.9512782,
+  lng: 136.1830219,
+};
+const goalOptions = {
+  strokeColor: '#00FF00',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#00FF00',
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 20,
+  zIndex: 1
+}
+const infoboxOptions = { closeBoxURL: '', enableEventPropagation: true };
+
 
 export const StampRally = (): React.ReactElement => {
   const viewModel = new StampRallyViewModel();
@@ -18,7 +44,27 @@ export const StampRally = (): React.ReactElement => {
       <HeaderS />
       <Divider section></Divider>
 
-      <Header>ルート</Header>
+      <Header>スタンプラリー</Header>
+
+      <LoadScript googleMapsApiKey="AIzaSyA21hP2aaQA54UNFQFq8he2G0REvzX6Vd4">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+  //        center={center}
+          center={center}
+          zoom={16}
+        >
+          {viewModel.stampRallySpots.map((item)=>
+            <><InfoBox options={infoboxOptions} position={item.coordinate}>
+              <div style={{ backgroundColor: 'red',opacity: 0.75,padding: 2 }}>
+                <div style={{ fontSize: 10,}}>{item.name}</div>
+              </div>
+            </InfoBox>
+            <Circle center={item.coordinate} options={goalOptions}></Circle></>
+          )}
+          {/*<Circle center={center} options={goalOptions}></Circle>*/}
+        </GoogleMap>
+      </LoadScript>
+
       <Container textAlign="center">
         <Image circular size="medium" src="images/animal.jpg" onClick={()=>{
           viewModel.stampEntrance();
