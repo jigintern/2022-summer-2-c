bc545c4bc63fc73bf5d5e6a2de8b85b193a24152
kitopito <kitopitowada@gmail.com>
2022-08-26 11:05:44 +0900

    stamp rally view model

--- src/view_model/StampRallyViewModel.ts
+++ src/view_model/StampRallyViewModel.ts
@@ -1,5 +1,5 @@
 import { getStoredRouteData, getStoredHotspotData } from "../function/GetStoredRouteData.tsx";
-import { useStampRallyReducer } from "../hooks/useStampRallyReducer.ts";
+import { ActionTypes, useStampRallyReducer } from "../hooks/useStampRallyReducer.ts";
 
 export class StampRallyViewModel {
     private state;
@@ -12,10 +12,41 @@ export class StampRallyViewModel {
     }
     
     get isEntranceVisited() {
+        console.log('ふがふが　エントランス押されたナリ');
         return this.state.isEntranceVisited;
+    }
+    get isLawnVisited() {
+        return this.state.isLawnVisited;
+    }
+    get isBellVisited() {
+        return this.state.isBellVisited;
+    }
+    get isZooVisited() {
+        return this.state.isZooVisited;
+    }
+    get isObservationTowerVisited() {
+        return this.state.isObservationTowerVisited;
+    }
+    get isGardenVisited() {
+        return this.state.isGardenVisited;
     }
 
     public stampEntrance() {
-//        dispatch({type: });
+        this.dispatch({type: ActionTypes.SET_ENTRANCE, data: true});
+    }
+    public stampLawn() {
+        this.dispatch({type: ActionTypes.SET_LAWN, data: true});
+    }
+    public stampBell() {
+        this.dispatch({type: ActionTypes.SET_BELL, data: true});
+    }
+    public stampZoo() {
+        this.dispatch({type: ActionTypes.SET_ZOO, data: true});
+    }
+    public stampObservationTower() {
+        this.dispatch({type: ActionTypes.SET_OBSERVATION_TOWER, data: true});
+    }
+    public stampGarden() {
+        this.dispatch({type: ActionTypes.SET_GARDEN, data: true});
     }
 }
